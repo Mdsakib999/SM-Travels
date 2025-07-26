0f29ff9a8d900a26f235bd37f801d17766dcef40
Md. Shahidul Islam <shanto57575@gmail.com>
2025-07-26 13:57:39 +0600

    fixed border issues

---
+++ src/utils/FlightBookingOptions.js
@@ -0,0 +1,19 @@
+export const titleOptions = ["Mr", "Mrs", "Ms", "Dr", "Prof"];
+export const nationalityOptions = [
+  "Bangladeshi",
+  "American",
+  "British",
+  "Canadian",
+  "Australian",
+  "German",
+  "French",
+];
+export const countryOptions = [
+  "Bangladesh",
+  "United States",
+  "United Kingdom",
+  "Canada",
+  "Australia",
+  "Germany",
+  "France",
+];
